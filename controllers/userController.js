75f77f0d4005e1f45dcf0869f1d7959758a953a4
Ilmo10 <necok_cley@hotmail.com>
2022-07-11 09:40:34 -0300

    corrigindo bug passando uma function como parametro

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 class UserController {
 
-    constructor (formId, tableId){
+    constructor(formId, tableId) {
 
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
@@ -9,7 +9,7 @@ class UserController {
 
     }
 
-    onSubmit(){
+    onSubmit() {
 
         this.formEl.addEventListener("submit", event => {
 
@@ -17,91 +17,111 @@ class UserController {
 
             let values = this.getValues();
 
-            this.getPhoto((content) => {
+            this.getPhoto().then(
+                (content)=>{
 
-                values.photo = content;
+                    values.photo = content;
 
                 this.addLine(values);
+
+                },
+                (e)=>{
+                    console.error(e);
+                }
+            );
+
+        });
+
+    }
+
+    getPhoto() {
+
+        return new Promise((resolve, reject)=>{
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+                }
+
             });
 
-        
-        });
+            let file = elements[0].files[0];
 
-    }
+            fileReader.onload = () => {
 
-    getPhoto(){
+                resolve(fileReader.result);
 
-        let fileReader = new FileReader();
+            };
 
-        let elements = [...this.formEl.elements].filter(item => {
-
-            if (item.name === 'photo') {
-                return item;
+            fileReader.onerror = (e) => {
+                reject(e);
             }
+            if(file){
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            };
 
         });
-
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-
-            callback(fileReader.result);
-
-        };
-
-        fileReader.readAsDataURL(file);
-
     }
 
-    getValues(){
+    getValues() {
 
         let user = {};
 
-        [...this.formEl.elements].forEach(function(field, index){
+        [...this.formEl.elements].forEach(function (field, index) {
 
             if (field.name === "gender") {
-    
+
                 if (field.checked) {
                     user[field.name] = field.value
                 }
-    
+
+            } else if(field.name == "admin") {
+
+                user[field.name] = field.checked;
+
             } else {
-    
+
                 user[field.name] = field.value
-    
+
             }
-    
+
         });
-    
+
         return new User(
-            user.name, 
-            user.gender, 
-            user.birth, 
-            user.country, 
-            user.email, 
-            user.password, 
-            user.photo, 
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
             user.admin
         );
 
     }
 
-    
+
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
                 <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${dataUser.birth}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-            </tr>
-        `;
+                `;
+        this.tableEl.appendChild(tr);
 
     }
 
